ebdb6c1ee1d7ba4c27adea610cb55f9bc0053764
awais512 <raza.awais25@gmail.com>
2021-05-23 15:27:27 +0500

    Get single Room

--- controllers/roomController.js
+++ controllers/roomController.js
@@ -1,17 +1,5 @@
 const Room = require('../models/room');
 
-// @route   GET /api/rooms
-// @desc    Create new Room
-// @access  Public
-const allRooms = async (req, res) => {
-  try {
-    const rooms = await Room.find({});
-    res.status(200).json({ success: true, count: rooms.length, rooms });
-  } catch (error) {
-    res.status(400).json({ success: false, error: error.message });
-  }
-};
-
 // @route   POST /api/rooms
 // @desc    Create new Room
 // @access  Private
@@ -24,4 +12,33 @@ const createRoom = async (req, res) => {
   }
 };
 
-export { allRooms, createRoom };
+// @route   GET /api/rooms
+// @desc    Create new Room
+// @access  Public
+const allRooms = async (req, res) => {
+  try {
+    const rooms = await Room.find({});
+    res.status(200).json({ success: true, count: rooms.length, rooms });
+  } catch (error) {
+    res.status(400).json({ success: false, error: error.message });
+  }
+};
+
+// @route   GET /api/rooms/:id
+// @desc    Create new Room
+// @access  Public
+const getRoom = async (req, res) => {
+  try {
+    const room = await Room.findById(req.query.id);
+    if (!room) {
+      return res
+        .status(404)
+        .json({ success: false, message: 'Room does not exist' });
+    }
+    res.status(200).json({ success: true, room });
+  } catch (error) {
+    res.status(400).json({ success: false, error: error.message });
+  }
+};
+
+export { allRooms, createRoom, getRoom };
