690179e903d6b309258c847ba14f5ac157ef98de
awais512 <raza.awais25@gmail.com>
2021-05-25 21:29:47 +0500

    All Rooms actions

---
+++ redux/actions/roomAction.js
@@ -0,0 +1,31 @@
+import {
+  ALL_ROOMS_SUCCESS,
+  ALL_ROOMS_FAILED,
+  CLEAR_ERRORS,
+} from '../constants/roomConstants';
+import axios from 'axios';
+import absoluteUrl from 'next-absolute-url';
+
+//Get all rooms
+export const getRooms = (req) => async (dispatch) => {
+  try {
+    const { origin } = absoluteUrl(req);
+    const { data } = await axios.get(`${origin}/api/rooms`);
+    dispatch({
+      type: ALL_ROOMS_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: ALL_ROOMS_FAILED,
+      payload: error.response.data.message,
+    });
+  }
+};
+
+//Clear Errors
+export const getRooms = () => async (dispatch) => {
+  dispatch({
+    type: CLEAR_ERRORS,
+  });
+};
