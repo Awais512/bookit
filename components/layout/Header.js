072ce62cf5392cd734221567269d1dc74acdfef4
awais512 <raza.awais25@gmail.com>
2021-05-25 15:03:38 +0500

    Homepage setup

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
         <div className='col-3 p-0'>
           <div className='navbar-brand'>
             <img
-              style={{ cursor: pointer }}
+              style={{ cursor: 'pointer' }}
               src='/images/bookit_logo.png'
               alt='BookIT'
             />
