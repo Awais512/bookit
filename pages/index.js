0922178a33993ab0e70cd7cf3f7c5131fcd6972e
awais512 <raza.awais25@gmail.com>
2021-05-27 17:20:37 +0500

    Search component additional option

--- pages/index.js
+++ pages/index.js
@@ -15,6 +15,8 @@ export default function Index() {
 
 export const getServerSideProps = wrapper.getServerSideProps(
   async ({ req, query, store }) => {
-    await store.dispatch(getRooms(req, query.page, query.location));
+    await store.dispatch(
+      getRooms(req, query.page, query.location, query.guests, query.category)
+    );
   }
 );
