07d994129ff8c212634e0fa0327c40e7f642de6b
awais512 <raza.awais25@gmail.com>
2021-05-31 09:27:57 +0500

    Setting up cloudinary

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,24 @@
 import User from '../models/user';
 import ErrorHandler from '../utils/errorHandler';
 import catchAsync from '../middlewares/catchAsyncErrors';
+import cloudinary from 'cloudinary';
+
+//Setting up cloudinary config
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 // @route   POST /api/rooms
 // @desc    Create new Room
 // @access  Private
 const registerUser = catchAsync(async (req, res, next) => {
+  const result = await cloudinary.v2.uploader.upload(req.body.avatar, {
+    folder: 'bookit/avatars',
+    width: '150',
+    crop: 'scale',
+  });
   const { name, email, password } = req.body;
 
   const user = await User.create({
@@ -13,8 +26,8 @@ const registerUser = catchAsync(async (req, res, next) => {
     email,
     password,
     avatar: {
-      public_id: 'PUBLIC_ID',
-      url: 'URL',
+      public_id: result.public_id,
+      url: result.secure_url,
     },
   });
   res
--- next.config.js
+++ next.config.js
@@ -1,12 +1,10 @@
-// module.exports = {
-//   env: {
-//     DB_LOCAL_URI: 'mongodb://localhost:27017/bookit',
-//   },
-// };
-
 module.exports = {
   env: {
     DB_LOCAL_URI: 'mongodb://localhost:27017/bookit',
+    CLOUDINARY_CLOUD_NAME: 'awais512',
+    CLOUDINARY_API_KEY: '664727195549493',
+    CLOUDINARY_SECRET_KEY:
+      'CLOUDINARY_URL=cloudinary://664727195549493:FKnl48TdaY6sWCyEAQIwJwUqzbg@awais512',
   },
   images: {
     domains: ['res.cloudinary.com'],
